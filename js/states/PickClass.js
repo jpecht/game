5bfa4d0afb6ab2547b46ff5832098ab3fa96f234
Jefferson <me.jefferson@gmail.com>
2014-07-22 23:03:04 -0400

    improved pick class screen

--- js/states/PickClass.js
+++ js/states/PickClass.js
@@ -10,15 +10,58 @@ Game.PickClass.prototype = {
 		/*this.music = this.add.audio('titleMusic');
 		this.music.play();*/
 
-        this.player_classes.push(this.add.sprite(50, 25, 'class_builder'));
-        this.player_classes.push(this.add.sprite(425, 25, 'class_ninja'));
-        this.player_classes.push(this.add.sprite(50, 275, 'class_merchant'));
-        this.player_classes.push(this.add.sprite(425, 275, 'class_tank'));
+        var builder = this.add.sprite(50, 25, 'class_builder');
+        var ninja = this.add.sprite(425, 25, 'class_ninja');
+        var merchant = this.add.sprite(50, 275, 'class_merchant');
+        var tank = this.add.sprite(425, 275, 'class_tank');
+
+        builder.stats = {
+            hp: 5,
+            speed: 200,
+            build_speed_mod: 95,
+            price_mod: 100            
+        };
+        ninja.stats = {
+            hp: 5,
+            speed: 210,
+            build_speed_mod: 100,
+            price_mod: 100            
+        };
+        merchant.stats = {
+            hp: 5,
+            speed: 200,
+            build_speed_mod: 100,
+            price_mod: 95
+        };
+        tank.stats = {
+            hp: 6,
+            speed: 200,
+            build_speed_mod: 100,
+            price_mod: 100            
+        };
+
+        this.player_classes = [builder, ninja, merchant, tank];
+        var names = ['BUILDER', 'NINJA', 'MERCHANT', 'TANK'];
+
         for (var i = 0; i < this.player_classes.length; i++) {
-            sprite = this.player_classes[i];
+            var sprite = this.player_classes[i];
             sprite.inputEnabled = true;
             sprite.alpha = 0.5;
-       }
+
+            var name = this.add.text(sprite.width/2, 50, names[i], {font: '14px Arial'});
+            name.anchor.setTo(0.5, 0.5);
+            sprite.addChild(name);
+
+            var health = this.add.text(50, 90, 'Health: ' + sprite.stats.hp, {font: '14px Arial'});
+            var speed = this.add.text(50, 105, 'Speed: ' + sprite.stats.speed, {font: '14px Arial'});
+            var build_speed = this.add.text(50, 120, 'Build Speed: ' + 1/sprite.stats.build_speed_mod, {font: '14px Arial'});
+            var price_mod = this.add.text(50, 135, 'Price Modifier: ' + sprite.stats.price_mod, {font: '14px Arial'});
+            sprite.addChild(health);
+            sprite.addChild(speed);
+            sprite.addChild(build_speed);
+            sprite.addChild(price_mod);
+        }
+
 
         this.text1 = this.add.text(this.world.centerX, this.world.height-50, 'Pick a class', {
         	font: '24px Arial'
